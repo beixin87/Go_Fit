d20d0866dedac1bd7149dfc873926d176345f6d0
Xin Bei <beixin87@GMAIL.com>
2017-11-20 23:08:50 -0800

    Using scaffold to generate a course model for beginning

---
+++ db/migrate/20171121070724_create_courses.rb
@@ -0,0 +1,12 @@
+class CreateCourses < ActiveRecord::Migration
+  def change
+    create_table :courses do |t|
+      t.string :name
+      t.integer :instructor_id
+      t.integer :limit
+      t.integer :fee
+
+      t.timestamps null: false
+    end
+  end
+end
