708a3629fa12342ad448e4c7b049100ef5f35ad4
coleung <leonleung96@gmail.com>
2017-11-24 16:16:39 -0800

    updated

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,5 +1,14 @@
 class CalculatorController < ApplicationController
   def index
     @user = current_user
+    @age = age(@user.date_of_birth)
+  end
+
+private
+  def age(date_of_birth)
+    today = Date.today
+    age = today.year - date_of_birth.year
+    age -= 1 if date_of_birth.strftime("%m%d").to_i > today.strftime("%m%d").to_i
+    age
   end
 end
