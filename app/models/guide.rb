b6070d80d2f75bc46b5c429bc431574b2c79de44
Rick Ma <sma161@sfu.ca>
2017-11-29 20:31:59 -0800

    fix search

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -4,8 +4,6 @@ class Guide < ActiveRecord::Base
   validates :title, presence: true
   validates :content, presence: true
 def self.search(search)
-  where("title ILIKE ?", "%#{search}%") 
-  where("content ILIKE ?", "%#{search}%")
-  where("user_name ILIKE ?", "%#{search}%")
+  where("title ILIKE ?", "%#{search}%")  || where("content ILIKE ?", "%#{search}%") ||  where("user_name ILIKE ?", "%#{search}%")
 end
 end
