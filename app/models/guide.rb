3b57b2294f2e97e84b18e54c000010d42b2e6b1e
Rick Ma <sma161@sfu.ca>
2017-11-29 21:11:10 -0800

    fix search

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,10 +1,10 @@
 class Guide < ActiveRecord::Base
   belongs_to :user
   has_many :images
-  validates :title, presence: true
-  validates :content, presence: true
   def self.search(search)
   	where("content ILIKE ?", "%#{search}%")
-  	where("title ILIKE ?", "%#{search}%") 	
-  end
+  	where("title ILIKE ?", "%#{search}%")
+  end 
+  validates :title, presence: true
+  validates :content, presence: true	 
 end
