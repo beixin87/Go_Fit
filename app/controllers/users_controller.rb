7c24c1c35e59e0112e4e458942512d69267dd0fb
Rick Ma <sma161@sfu.ca>
2017-11-30 00:07:13 -0800

    fixing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
 
   def index
        @userCnt = User.all.count 
-      @users = User.order('id DESC').paginate(page: params[:page], per_page: 10)
+      #@users = User.order('id DESC').paginate(page: params[:page], per_page: 10)
+      @users =User.all
   end
 
 
