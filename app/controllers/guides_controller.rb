e58bf15d7d580331f96dcea64dcc7ba6d67f5dd5
Rick Ma <sma161@sfu.ca>
2017-11-29 19:34:29 -0800

    fix search

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,5 +1,5 @@
 class GuidesController < ApplicationController
-   before_action :logged_in_user, only: [:index, :show, :new, :search :create, :edit, :update, :destroy]
+   before_action :logged_in_user, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
 
   def index         
