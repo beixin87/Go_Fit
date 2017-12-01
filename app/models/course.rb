44399418e1c351a1ef8af19f97345bb9a5f404b7
Xin Bei <beixin87@GMAIL.com>
2017-11-30 18:30:25 -0800

    Fix: one student can join the same course more than once

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,20 +2,18 @@ class Course < ActiveRecord::Base
   has_many :user_courses
   has_many :users, through: :user_courses
 
-  def can_add_student?(course_id)
-    under_course_limit?(course_id) && !student_already_added?(user_id)
-
-  end
-
-  def under_course_limit?(course_id)
-    course = Course.find(course_id )
-    (user_courses.count < course.limit)
-  end
-
   def student_already_added?(user_id)
     student = User.find(user_id)
-
-
     user_courses.where(user_id: student.id).exists?
   end
+
+  def under_course_limit?
+    user_courses.count < limit
+  end
+
+
+  def can_add_student?(user_id)
+    under_course_limit? && !student_already_added?(user_id)
+  end
+
 end
