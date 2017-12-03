60a717de7aa0502b0198f04756a13e5d3683a28c
Xin Bei <beixin87@GMAIL.com>
2017-12-03 00:16:13 -0800

    connect gym , course, user type relations

--- db/migrate/20171129085545_add_user_id_to_gyms.rb
+++ db/migrate/20171129085545_add_user_id_to_gyms.rb
@@ -1,6 +1,7 @@
 class AddUserIdToGyms < ActiveRecord::Migration
   def change
     add_column :gyms, :user_id, :integer
+    add_column :gyms, :course_id, :integer
     add_index :gyms, :user_id
   end
 end
