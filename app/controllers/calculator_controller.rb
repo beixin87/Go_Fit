e9bc8b4815b799d09733edecf24e029a6ba61043
Xin Bei <beixin87@GMAIL.com>
2017-12-04 21:52:09 -0800

    pull new version

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,4 +1,5 @@
 class CalculatorController < ApplicationController
+  before_action :logged_in_user, only: [:show]
 
   def show
     @user = current_user
@@ -18,4 +19,10 @@ private
     end
   end
 
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
 end
--- app/controllers/user_courses_controller.rb
+++ app/controllers/user_courses_controller.rb
@@ -32,7 +32,7 @@ class UserCoursesController < ApplicationController
     respond_to do |format|
       if @user_course.save
         @course = Course.find(params[:course_id])
-        @course.numberofenrolled = @course.user_courses.count
+        
         @user.update_attributes(type: "Student")
         format.html { redirect_to request.referrer, notice: 'Course was successfully added.' }
         format.json { render :show, status: :created, location: @user_course }
