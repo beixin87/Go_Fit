01efb56e1aed4e2f750b4cd8051bd837c2deeb26
xuecongt <xuecongt@sfu.ca>
2017-12-04 21:07:44 -0800

    fixed footer issues

--- db/schema.rb
+++ db/schema.rb
@@ -16,13 +16,10 @@ ActiveRecord::Schema.define(version: 20171203202807) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "average_caches", force: :cascade do |t|
-    t.integer  "rater_id"
-    t.integer  "rateable_id"
-    t.string   "rateable_type"
-    t.float    "avg",           null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
+    t.string   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "calculators", force: :cascade do |t|
@@ -52,16 +49,6 @@ ActiveRecord::Schema.define(version: 20171203202807) do
     t.integer  "guide_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "fbusers", force: :cascade do |t|
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "name"
-    t.string   "oauth_token"
-    t.datetime "oauth_expires_at"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
   end
 
   create_table "guides", force: :cascade do |t|
@@ -108,66 +95,6 @@ ActiveRecord::Schema.define(version: 20171203202807) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "overall_averages", force: :cascade do |t|
-    t.integer  "rateable_id"
-    t.string   "rateable_type"
-    t.float    "overall_avg",   null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.integer  "topic_id"
-    t.integer  "user_id"
-    t.text     "text"
-    t.integer  "rating"
-    t.boolean  "anon"
-    t.integer  "parent"
-    t.integer  "postable_id"
-    t.string   "postable_type"
-    t.boolean  "deleted"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
-  create_table "rates", force: :cascade do |t|
-    t.integer  "rater_id"
-    t.integer  "rateable_id"
-    t.string   "rateable_type"
-    t.float    "stars",         null: false
-    t.string   "dimension"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "rates", ["rateable_id", "rateable_type"], name: "index_rates_on_rateable_id_and_rateable_type", using: :btree
-  add_index "rates", ["rater_id"], name: "index_rates_on_rater_id", using: :btree
-
-  create_table "rating_caches", force: :cascade do |t|
-    t.integer  "cacheable_id"
-    t.string   "cacheable_type"
-    t.float    "avg",            null: false
-    t.integer  "qty",            null: false
-    t.string   "dimension"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "rating_caches", ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type", using: :btree
-
-  create_table "topics", force: :cascade do |t|
-    t.string   "description"
-    t.string   "title"
-    t.integer  "course_id"
-    t.integer  "user_id"
-    t.text     "first_post"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "topics", ["course_id"], name: "index_topics_on_course_id", using: :btree
-  add_index "topics", ["user_id"], name: "index_topics_on_user_id", using: :btree
 
   create_table "user_courses", force: :cascade do |t|
     t.integer  "user_id"
